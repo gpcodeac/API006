a3ea83bb347050d369bcb6905fba2955c1aada4a
gpcodeac <pashkovskij@gmail.com>
2024-04-11 18:39:32 +0300

    asd

--- API006/Database/Repositories/Interfaces/IAccountRepository.cs
+++ API006/Database/Repositories/Interfaces/IAccountRepository.cs
@@ -4,9 +4,9 @@ namespace API006.Database.Repositories.Interfaces
 {
     public interface IAccountRepository
     {
-         Account? Withdraw(string accountNumber, int amount);
-         Account? Delete(string accountNumber);
-         Account? Deposit(string accountNumber, int amount);
+        Account? Withdraw(string accountNumber, int amount);
+        Account? Delete(string accountNumber);
+        Account? Deposit(string accountNumber, int amount);
         Account? GetAccount(string accountNumber);
         List<Account>? GetAccount(int userId);
         void AddAccount(Account account);
